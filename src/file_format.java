ec612032cdf2e0eb834d15d0c85517323114fe19
Andrew Faraponov <andrey.faraponov@gmail.com>
2017-08-23 18:55:36 +0300

    little changes && test git

--- src/file_format.java
+++ src/file_format.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class file_format
 {
 	public static void main(String[] args) 
-	{	
+	{
 		Scanner sc = new Scanner(System.in);
 		String f_name = sc.nextLine();
 		f_name = f_name.replace(' ', '.');
